9a59fd45580e344b3c19702f2914c3aecbf0f470
Khóa Xuân <89636123+RuleOfNight@users.noreply.github.com>
2024-11-02 19:44:45 +0700

    Update Main.py
    change to work

--- Main.py
+++ Main.py
@@ -7,7 +7,12 @@ from discord.ext import commands
 
 TOKEN = '' #Cất phòng khi bay token con bot
 
-client = discord.Client()
+# Discord mới đổi từ năm 2022 thì phải, nay lôi con bot lên upd
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+client = commands.Bot(command_prefix='!', intents=intents)
+
 
 
 @client.event
